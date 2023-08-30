20ef310c7563b23db16dce572acee2dd328c34ee
Javier Ibanez <Expertprep2015@gmail.com>
2023-08-29 23:52:19 -0400

    setting up my project section

--- portfolio/src/app/projects/projects.component.ts
+++ portfolio/src/app/projects/projects.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.sass']
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      image: 'path-to-image-1.jpg',
+      title: 'Project 1',
+      description: 'Description of Project 1'
+    },
+    {
+      image: 'path-to-image-2.jpg',
+      title: 'Project 2',
+      description: 'Description of Project 2'
+    },
+    // Add more project objects as needed
+  ];
 }

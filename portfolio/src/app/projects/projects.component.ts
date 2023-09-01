afc38e764e78fb62559179030391c76c296d6ebd
Javier Ibanez <Expertprep2015@gmail.com>
2023-09-01 15:35:22 -0400

    added background image and moved the about me component to the top of the main page

--- portfolio/src/app/projects/projects.component.ts
+++ portfolio/src/app/projects/projects.component.ts
@@ -9,12 +9,12 @@ export class ProjectsComponent {
   projects = [
     {
       image: 'assets/spartan_soccer_sc.png',
-      title: 'Project 1',
-      description: 'Description of Project 1'
+      title: 'Spartan U8 Wesite',
+      description: "Leveraging Flask, a Python micro web framework, as the backend foundation, the site employs HTML, CSS, and JavaScript to structure, style, and enhance its web pages. The responsive design is achieved through Bootstrap, ensuring the website's adaptability across various devices. The interactive calendar is powered by the FullCalendar JavaScript library, enabling users to stay updated on team events. Integrating the Google Sheets API facilitates real-time data retrieval and display from a Google Sheets spreadsheet. The Jinja2 templating engine dynamically renders content, enhancing the site's flexibility. Finally, hosting on the Render platform ensures reliable and efficient deployment."
     },
     {
       image: 'assets/recipeapp.png',
-      title: 'Project 2',
+      title: 'Recipe Website',
       description: 'Description of Project 2'
     },
     // Add more project objects as needed

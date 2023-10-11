eaa29bb5cfd79cf19a7c37b71a2ace915ab38ff0
Javier Ibanez <Expertprep2015@gmail.com>
2023-10-11 11:27:59 -0400

    working on UI

--- portfolio/src/app/projects/projects.component.ts
+++ portfolio/src/app/projects/projects.component.ts
@@ -17,7 +17,7 @@ export class ProjectsComponent {
       image: 'assets/spartan_soccer_sc.png',
       title: 'Spartan U8 Soccer',
       website: 'https://u8-spartan.onrender.com/',
-      description: "Leveraging Flask, a Python micro web framework, as the backend foundation, the site employs HTML, CSS, and JavaScript to structure, style, and enhance its web pages. The responsive design is achieved through Bootstrap, ensuring the website's adaptability across various devices. The interactive calendar is powered by the FullCalendar JavaScript library, enabling users to stay updated on team events. Integrating the Google Sheets API facilitates real-time data retrieval and display from a Google Sheets spreadsheet. The Jinja2 templating engine dynamically renders content, enhancing the site's flexibility. Finally, hosting on the Render platform ensures reliable and efficient deployment."
+      description: "Leveraging Flask as the backend foundation, the site employs HTML, CSS, and JavaScript to structure, style, and enhance its web pages. The responsive design is achieved through Bootstrap, ensuring the website's adaptability across various devices. The interactive calendar is powered by the FullCalendar JavaScript library. Integrating the Google Sheets API facilitates real-time data retrieval and display from a Google Sheets spreadsheet. Hosting on the Render platform ensures reliable and efficient deployment."
     },
     // Add more project objects as needed
   ];

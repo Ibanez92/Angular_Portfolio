3af6316eccb195407e2e893f93a08d3e50a20e4d
Javier Ibanez <Expertprep2015@gmail.com>
2023-10-19 16:28:42 -0400

    working on text and git pages deployment

--- portfolio/src/app/projects/projects.component.ts
+++ portfolio/src/app/projects/projects.component.ts
@@ -11,13 +11,13 @@ export class ProjectsComponent {
       image: 'assets/tictactoe_wide.png',
       title: 'TicTacToe',
       website: 'https://tictactoe-e327a.web.app/',
-      description: "I have created an engaging TicTacToe application using the Angular framework in JavaScript. Leveraging the power of Angular, I have built a dynamic and interactive gaming experience. The application is deployed as a Progressive Web Application (PWA) on the Firebase platform, providing seamless accessibility across devices. Users can conveniently install the game on their desktops with the browser's install button, ensuring a smooth transition to a desktop-like experience. This project showcases my knowledge in Angular, JavaScript, and PWA development, delivering a captivating and user-friendly gaming application." 
+      description: "TicTacToe game using the Angular framework in JavaScript. The application is deployed as a Progressive Web Application (PWA) on the Firebase platform, providing seamless accessibility across devices. Users can conveniently install the game on their desktops with the browser's install button, ensuring a smooth transition to a desktop-like experience." 
     },
     {
       image: 'assets/spartan_soccer_sc.png',
       title: 'Spartan U8 Soccer',
       website: 'https://u8-spartan.onrender.com/',
-      description: "Leveraging Flask as the backend foundation, the site employs HTML, CSS, and JavaScript to structure, style, and enhance its web pages. The responsive design is achieved through Bootstrap, ensuring the website's adaptability across various devices. The interactive calendar is powered by the FullCalendar JavaScript library. Integrating the Google Sheets API facilitates real-time data retrieval and display from a Google Sheets spreadsheet. Hosting on the Render platform ensures reliable and efficient deployment."
+      description: "Leveraging Flask as the backend foundation, the site employs HTML, CSS, and JavaScript to structure, style, and enhance its web pages. The responsive design is achieved through Bootstrap. The calendar is powered by the FullCalendar JavaScript library. Integrating the Google Sheets API facilitates real-time data retrieval and display from a Google Sheets spreadsheet. Hosted on Render platform."
     },
     // Add more project objects as needed
   ];
